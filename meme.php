68fefb04aa663065be72204ac294db3082914bfe
Utsab <utsab.k.saha@gmail.com>
2018-10-05 09:39:17 +0000

    Display memes

--- meme.php
+++ meme.php
@@ -13,6 +13,25 @@ function createMeme($line1, $line2) {
     $statement->execute(); 
 }
 
+function displayMemes() {
+    
+    $dbConn = getDatabaseConnection(); 
+
+    $sql = "SELECT * from all_memes"; 
+    $statement = $dbConn->prepare($sql); 
+    
+    $statement->execute(); 
+    $records = $statement->fetchAll(); 
+    
+    foreach ($records as $record) {
+        echo '<div class="meme-div" style="background-image:url(https://upload.wikimedia.org/wikipedia/commons/f/ff/Deep_in_thought.jpg);">'; 
+        echo "<h2 class='line1'>".$record['line1']."</h2>"; 
+        echo "<h2 class='line2'>".$record['line2']."</h2>"; 
+        echo "</div>"; 
+    }
+}
+
+
 
 if (isset($_POST['line1']) && isset($_POST['line2'])) {
   createMeme($_POST['line1'], $_POST['line2']); 
@@ -27,10 +46,10 @@ if (isset($_POST['line1']) && isset($_POST['line2'])) {
   <head>
     <title>A Meme</title>
     <style>
-      #meme-div{
+      .meme-div{
       width: 450px;
       height: 450px;
-      background-size: 100%;
+      background-size: cover;
       text-align: center;
       position: relative;
       }
@@ -44,21 +63,34 @@ if (isset($_POST['line1']) && isset($_POST['line2'])) {
         color: white;
         text-shadow: 1px 1px black;
       }
-      #line1 {
+      .line1 {
          top: 0;
        }
-      #line2 {
+      .line2 {
          bottom: 0;
        }
+       
+       
+       
+       .memes-container .meme-div {
+         float: left;
+         margin: 0px 20px;
+       }
+       
     </style>
   </head>
   <body>
     <h1>Your Meme</h1>
     <!--The image needs to be rendered for each new meme
     so set the div's background-image property inline -->
-    <div id="meme-div" style="background-image:url(https://upload.wikimedia.org/wikipedia/commons/f/ff/Deep_in_thought.jpg);">
-      <h2 id="line1"><?= $_POST['line1'] ?> </h2>
-      <h2 id="line2"><?= $_POST['line2'] ?></h2>
+    <div class="meme-div" style="background-image:url(https://upload.wikimedia.org/wikipedia/commons/f/ff/Deep_in_thought.jpg);">
+      <h2 class="line1"><?= $_POST['line1'] ?> </h2>
+      <h2 class="line2"><?= $_POST['line2'] ?></h2>
+    </div>
+    
+    <h1>All memes</h1>
+    <div class="memes-container">
+      <?php displayMemes(); ?>
     </div>
   </body>
 </html>
